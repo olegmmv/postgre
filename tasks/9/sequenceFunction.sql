c5b9d0a547a1a6577279b3300b4dba1f9e62d0ae
Oleg Mamiev <olegios12@gmail.com>
2020-05-22 15:14:26 +0300

    task 9

--- tasks/9/sequenceFunction.sql
+++ tasks/9/sequenceFunction.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE FUNCTION getSequence(n INT) 
 RETURNS TABLE (num INT) AS $$ DECLARE i INT;
 BEGIN 
-  CREATE temp TABLE t(num INT);
-  FOR i IN 1..n LOOP 
-    INSERT INTO t VALUES (i);
-  END LOOP;
-  RETURN query 
-  SELECT * FROM t;
-  DROP TABLE t;
+    CREATE temp TABLE t(num INT);
+    FOR i IN 1..n LOOP 
+        INSERT INTO t VALUES (i);
+    END LOOP;
+    RETURN query 
+    SELECT * FROM t;
+    DROP TABLE t;
 END;
 $$ LANGUAGE plpgsql;
 
