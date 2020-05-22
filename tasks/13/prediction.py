6a2eb6599305ac9b4f4f8022d5fdebfe1c6b5591
Oleg Mamiev <olegios12@gmail.com>
2020-05-22 22:22:40 +0300

    task 13

--- tasks/13/prediction.py
+++ tasks/13/prediction.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from sqlalchemy import create_engine
 
-base = 'localhost: 5432/db'
+base = 'localhost:5432/db'
 name_pass = 'login:password'
 pref = 'postgresql'
 engine = create_engine(f'{pref}://{name_pass}@{base}')
@@ -10,8 +10,8 @@ engine = create_engine(f'{pref}://{name_pass}@{base}')
 def moving_average(dateFrom: str, dateTo: str, window_size: int) -> pd.DataFrame:
     SQL = '''
         SELECT
-           city.region,
-           recept.ddate,
+           city.region AS reg,
+           recept.ddate AS dd,
            SUM(goods.weight * recgoods.volume) AS s
         FROM city 
            JOIN client ON client.city = city.id 
@@ -34,10 +34,19 @@ def moving_average(dateFrom: str, dateTo: str, window_size: int) -> pd.DataFrame
     if (N < window_size):
         raise ValueError(f'Invalid windows size > {N}')
 
-    moving_average = df['s'].rolling(window=window_size).mean()
+    dfs = df.set_index(['reg'])
+    dfs.drop('dd', axis=1, inplace=True)
+    dfs = dfs.groupby(level=['reg']).rolling(
+        window=window_size,  min_periods=1
+    ).mean()
+    dfs.reset_index(level=[2, 1], inplace=True)
+    dfs.reset_index(drop=True, inplace=True)
+    dft = df.sort_values(['reg'],)
+    dft.reset_index(drop=True, inplace=True)
     names = ['region', 'date', 'sum']
-    df.columns = names
-    df['prediction'] = moving_average
-    df.to_sql('prediction', engine, if_exists='replace')
+    dft.columns = names
+    dft['prediction'] = dfs['s']
+    dft = dft.sort_values('date')
 
+    df.to_sql('prediction', engine, if_exists='replace')
     return df
