849c46bb20010bfc87aed5169866184190f1bbd8
Oleg Mamiev <olegios12@gmail.com>
2020-05-22 15:11:57 +0300

    task 6

--- tasks/6/twoWeeksPeriod.sql
+++ tasks/6/twoWeeksPeriod.sql
@@ -1,34 +1,33 @@
 WITH date_t AS
-  (SELECT '2020-03-01'::date + day AS ddate
-   FROM generate_series(0, 13) day)
+    (SELECT '2020-03-01'::date + day AS ddate
+    FROM generate_series(0, 13) day)
 SELECT incs.ddate,
-       incs.storage,
-       incs.goods,
-       COALESCE(incs.inc_vol, 0) - COALESCE(recs.rec_vol, 0)       
+    incs.storage,
+    incs.goods,
+    COALESCE(incs.inc_vol, 0) - COALESCE(recs.rec_vol, 0)       
 FROM
-  (SELECT date_t.ddate,
-          inc.storage,
-          incgoods.goods,
-          SUM(incgoods.volume) inc_vol
-   FROM income inc
-   JOIN incgoods ON incgoods.id = inc.id
-   JOIN date_t ON date_t.ddate >= inc.ddate
-   GROUP BY date_t.ddate,
-            inc.storage,
-            incgoods.goods) AS incs
+    (SELECT date_t.ddate,
+        inc.storage,
+        incgoods.goods,
+        SUM(incgoods.volume) inc_vol
+    FROM income inc
+    JOIN incgoods ON incgoods.id = inc.id
+    JOIN date_t ON date_t.ddate >= inc.ddate
+    GROUP BY date_t.ddate,
+        inc.storage,
+        incgoods.goods) AS incs
 FULL OUTER JOIN
-  (SELECT date_t.ddate,
-          rec.storage,
-          recgoods.goods,
-          SUM(recgoods.volume) rec_vol
-   FROM recept rec
-   JOIN recgoods ON recgoods.id = rec.id
-   JOIN date_t ON date_t.ddate >= rec.ddate
-   GROUP BY date_t.ddate,
-            rec.storage,
-            recgoods.goods) AS recs 
+    (SELECT date_t.ddate,
+        rec.storage,
+        recgoods.goods,
+        SUM(recgoods.volume) rec_vol
+    FROM recept rec
+    JOIN recgoods ON recgoods.id = rec.id
+    JOIN date_t ON date_t.ddate >= rec.ddate
+    GROUP BY date_t.ddate,
+        rec.storage,
+        recgoods.goods) AS recs 
 ON recs.ddate = incs.ddate
 AND recs.storage = incs.storage
 AND recs.goods = incs.goods
-ORDER BY incs.ddate,
-         incs.storage
+ORDER BY incs.ddate, incs.storage
